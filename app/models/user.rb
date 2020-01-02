33e803e08a2da37fc27deafcd160a3f26053bbb9
kenta1183 <k.imai_1183@icloud.com>
2020-01-03 02:21:03 +0900

    create messages start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :group_users
+  has_many :groups, through: :group_users
+  has_many :messages
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :group_users
-  has_many :groups, through: :group_users
 end
